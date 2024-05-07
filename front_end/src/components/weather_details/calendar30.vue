c639c18ccc9722b4068f3dd418869cc7b3250605
charles2530 <2569337619@qq.com>
2024-05-07 19:24:22 +0800

    ljb06 debug

--- front_end/src/components/weather_details/calendar30.vue
+++ front_end/src/components/weather_details/calendar30.vue
@@ -1,39 +1,55 @@
-import CurrentWeather from './sub_components/CurrentWeather.vue';
 <template>
-    <div class="common-layout mx-10">
-        <el-container class="bg-blue-500">
-            <el-main class="bg-pink-100">
-                <el-calendar class=" rounded-lg  italic">
-                    <template #date-cell="{ data }">
-                        <p :class="data.isSelected ? 'is-selected' : ''">
-                            {{ data.day.split('-')[2] }} <!-- 只显示日期部分 -->
-                            <img class="weather-icon" src="@/assets/img/阴.png" alt="阴天">
-
-                            <!-- <div  class="weather-icon">{{ " 18-20℃ " }}</div> -->
-                            <span class="p-1 text-xl bg-blue-100 shadow-sm rounded-full">{{ "18-20℃" }}</span>
-                        </p>
-                    </template>
-                </el-calendar>
-            </el-main>
-            <el-aside class="bg-yellow-50" width="20%">
-                <CurrentWeather class="md:basis-3/5" :weather="weather" :city="city" :search="searchShow"> </CurrentWeather>
-            </el-aside>
-
-        </el-container>
-    </div>
+  <div class="common-layout mx-10">
+    <el-container class="bg-blue-500">
+      <el-main class="bg-pink-100">
+        <el-calendar class="rounded-lg italic">
+          <template #date-cell="{ data }">
+            <el-row>
+              <el-col :span="8">
+                <p :class="data.isSelected ? 'is-selected text-xl' : 'text-xl'">
+                  {{ data.day.split("-")[2] }}
+                </p>
+              </el-col>
+              <el-col :span="16">
+                <img
+                  class="weather-icon"
+                  src="@/assets/img/阴.png"
+                  alt="阴天"
+                />
+              </el-col>
+            </el-row>
+            <el-row>
+              <span class="p-1 text-xl bg-blue-100 shadow-sm rounded-full">{{
+                "18-20℃"
+              }}</span>
+            </el-row>
+          </template>
+        </el-calendar>
+      </el-main>
+      <el-aside class="bg-yellow-50" width="20%">
+        <CurrentWeather
+          class="md:basis-3/5"
+          :weather="weather"
+          :city="city"
+          :search="searchShow"
+        >
+        </CurrentWeather>
+      </el-aside>
+    </el-container>
+  </div>
 </template>
 <script lang="ts" setup>
-import CurrentWeather from '@/components/weather_details/sub_components/CurrentWeather.vue';
+import CurrentWeather from "@c/weather_details/sub_components/CurrentWeather.vue";
 const weather = ref({
-    icon: 404,
-    temp: 20,
-    text: "cloud"
+  icon: 404,
+  temp: 20,
+  text: "cloud",
 });
 const city = ref({
-    name: '北京市',
-    adm2: '昌平区'
+  name: "北京市",
+  adm2: "昌平区",
 });
-const searchShow = ref(false)
+const searchShow = ref(false);
 </script>
 <!-- <script>
 export default {
@@ -45,7 +61,7 @@ export default {
 
 <style>
 .is-selected {
-    color: #1989fa;
+  color: #1989fa;
 }
 
 /* .date-cell {
@@ -54,13 +70,12 @@ export default {
 } */
 
 .weather-icon {
-    position: relative;
-    bottom: 60px;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 60px;
-    /* 调整图片宽度 */
-    height: 60px;
-    /* 调整图片高度 */
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 40px;
+  /* 调整图片宽度 */
+  height: 40px;
+  /* 调整图片高度 */
 }
 </style>
