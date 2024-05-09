b1cc481876fd36c832aa5d99f05106122799cda9
Charles2530 <2569337619@qq.com>
2024-05-09 20:03:45 +0800

    loadCityinfo

--- back_end/weather/management/commands/loadCityId.py
+++ back_end/weather/management/commands/loadCityId.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 import csv
-from models import City2CityId, Pro2City
+from weather.models import City2CityId, Pro2City
 
 
 class Command(BaseCommand):
