93d6dee027da4e4a63f221f106a6c2242192cf30
Charles2530 <2569337619@qq.com>
2024-05-09 12:26:04 +0800

    all

--- back_end/notifications/serializers.py
+++ back_end/notifications/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from .models import Notification
+from .models import Notification, WeatherForecast, CitySubscription
 
 
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
+        fields = '__all__'
+
+class WeatherForecastSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WeatherForecast
+        fields = '__all__'
+
+class CitySubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CitySubscription
         fields = '__all__'
