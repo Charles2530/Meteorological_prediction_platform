8f305096a9ca9b0c3516d553e11ec5c38805b2e1
Charles2530 <2569337619@qq.com>
2024-05-09 22:50:22 +0800

    t

--- back_end/weather/management/commands/hourlyupdate.py
+++ back_end/weather/management/commands/hourlyupdate.py
@@ -3,16 +3,17 @@ from weather.models import HourlyWeather
 import json
 from datetime import datetime, timedelta
 import requests
+import pytz
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        weather = requests.get('https://devapi.qweather.com/v7/weather/72h', params={
+        weather = requests.get('https://devapi.qweather.com/v7/weather/24h', params={
             'key': '7ddb2459227b4d6993afff0b4ba574ff',
             'location': "101010100", ## Beijing
         })
 
         weather = json.loads(weather.content.decode('utf-8'))
-        print(weather)
+        # print(weather)
         for hourly in weather["hourly"]:
             date_time = datetime.fromisoformat(hourly["fxTime"])
             timezon = pytz.timezone('Asia/Shanghai')
@@ -20,6 +21,25 @@ class Command(BaseCommand):
 
             data = HourlyWeather(
                 fxTime = date_time,
+                temp = hourly["temp"],
+                icon = hourly["icon"],
+                text = hourly["text"],
+                wind360 = hourly["wind360"],
+                windDir = hourly["windDir"],
+                windScale = hourly["windScale"],
+                windSpeed = hourly["windSpeed"],
+                humidity = hourly["humidity"],
+                pop = hourly["pop"],
+                precip = hourly["precip"],
+                pressure = hourly["pressure"],
+                cloud = hourly["cloud"],
+                dew = hourly["dew"],
+            )
+
+            data.save()
+
+            data = HourlyWeather(
+                fxTime = date_time + timedelta(days = -1),
                 temp = hourly["temp"],
                 icon = hourly["icon"],
                 text = hourly["text"],
