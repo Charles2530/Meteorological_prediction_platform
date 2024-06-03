de5b0e7dcf2009a0fecdbd42934990ac740c27ce
Charles2530 <2569337619@qq.com>
2024-06-03 19:42:27 +0800

    commit

--- test.py
+++ test.py
@@ -1,15 +1,8 @@
 
 from bs4 import BeautifulSoup
 import csv
+import json
 from openpyxl import load_workbook
-
-        # try:
-        #     f = open(r'/root/Meteorological_prediction_platform/back_end/China-City-List-latest.csv', 'r', newline='', encoding='utf-8')
-        # except FileNotFoundError:
-        #     try:
-        #         f = open(r'D:\\Programing\\SoftwareEngineering\\Meteorological_prediction_platform\\back_end\\China-City-List-latest.csv', 'r', newline='', encoding='utf-8')
-        #     except FileNotFoundError:
-        #         pass
 
 f = open(r'/root/Meteorological_prediction_platform/back_end/China-City-List-latest.csv', 'r', newline='', encoding='utf-8')
 
@@ -17,7 +10,7 @@ reader = csv.reader(f)
 cnt = 0
 
 res = []
-
+print('[')
 for i, row in enumerate(reader):
     if i < 2:
         continue
@@ -29,11 +22,14 @@ for i, row in enumerate(reader):
         "label": city_n_area,
         "value": city_n_area,
     })
-
+    print(r'  { label: "' + city_n_area + r'", value: "' + city_n_area + r'" },')
     # try:
     # Cityinfo.cityName = cityName
     # Cityinfo.areaName = row[2]
     # print(row[2])
     # Cityinfo.location = "{:.2f},{:.2f}".format(float(row[11]), float(row[12]))
     # Cityinfo.save()
+
+print(']')
+js_data = json.dumps(res)
 
